df931c84454e76cbe58a9bdddd5bf844c349afe4
wayne-growing <40487804+wayne-growing@users.noreply.github.com>
2018-06-29 23:59:43 +0800

    Revert "first commit"
    This reverts commit 2ee759d8d5cda6b22b6054bcce5572aa91ea72a1.

--- Q1.sh.py
+++
@@ -1,6 +0,0 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-================== RESTART: C:/Users/RF/Downloads/Q1.sh.py ==================
-Input your file locationC:/Users/RF/Downloads
->>> 
